ca36cddf6c00012ad49cf1e02c9ef12dce2c56c9
John O'Brien <johnobrien.dev@gmail.com>
2024-04-14 21:17:55 -0400

    create components for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className=''>
       <Navbar />
+      <Outlet />
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom"
+
 export default function Navbar(){
     return(
-        <nav>navbar</nav>
+        <nav>
+            <Link to='/'>Home</Link>
+            <ul>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/music'>Music</Link></li>
+                <li><Link to='contact'>Contact</Link></li>
+            </ul>
+        </nav>
     )
 }
