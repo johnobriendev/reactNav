6a11d117182e0fb9fb293928b4a0730c428edcff
John O'Brien <johnobrien.dev@gmail.com>
2024-04-15 12:25:14 -0400

    modify media queries for nav button and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,18 +7,18 @@ export default function Navbar(){
    const [menuOpen, setMenuOpen] = useState(false);
    
     return(
-        <nav className="flex justify-between items-center bg-slate-900 text-white p-5 sticky top-0">
+        <nav className="flex flex-col items-start justify-between  bg-slate-900 text-white p-5 sticky top-0">
             <Link to='/' className="text-3xl">Home</Link>
-            <div>
-                <div className="text-2xl md:hidden cursor-pointer" onClick={() => setMenuOpen(!menuOpen)}>
+            
+                <div className="text-2xl md:hidden cursor-pointer absolute top-5 right-5" onClick={() => setMenuOpen(!menuOpen)}>
                     <RxHamburgerMenu />
                 </div>
-                <ul className={menuOpen ? "flex" : ""}>
+                <ul className={menuOpen ? "flex flex-col " : "hidden md:flex"}>
                     <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded'>About</NavLink></li>
                     <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded'>Music</NavLink></li>
                     <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded'>Contact</NavLink></li>
                 </ul>
-            </div>
+           
             
         </nav>
     )
