1f85e66ee72f048e1cd67cb3aaaab363e62999f8
John O'Brien <johnobrien.dev@gmail.com>
2024-04-15 10:02:47 -0400

    add state to nav button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,25 @@
 import { NavLink, Link } from "react-router-dom"
+import { RxHamburgerMenu } from "react-icons/rx";
+
+import React, { useState } from "react";
 
 export default function Navbar(){
+   const [menuOpen, setMenuOpen] = useState(false);
+   
     return(
         <nav className="flex justify-between items-center bg-slate-900 text-white p-5 sticky top-0">
             <Link to='/' className="text-3xl">Home</Link>
-            <ul className="flex gap-4 text-xl">
-                <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded'>About</NavLink></li>
-                <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded'>Music</NavLink></li>
-                <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded'>Contact</NavLink></li>
-            </ul>
+            <div>
+                <div className="text-2xl md:hidden cursor-pointer" onClick={() => setMenuOpen(!menuOpen)}>
+                    <RxHamburgerMenu />
+                </div>
+                <ul className={menuOpen ? "flex" : ""}>
+                    <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded'>About</NavLink></li>
+                    <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded'>Music</NavLink></li>
+                    <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded'>Contact</NavLink></li>
+                </ul>
+            </div>
+            
         </nav>
     )
 }
