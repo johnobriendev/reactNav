4dabd1f1f5d8272ee34ea29f122174d03ec299a8
John O'Brien <johnobrien.dev@gmail.com>
2024-04-15 09:15:11 -0400

    add sticky to navbar and lorem text to about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { NavLink, Link } from "react-router-dom"
 
 export default function Navbar(){
     return(
-        <nav className="flex justify-between items-center bg-slate-900 text-white p-5">
+        <nav className="flex justify-between items-center bg-slate-900 text-white p-5 sticky top-0">
             <Link to='/' className="text-3xl">Home</Link>
             <ul className="flex gap-4 text-xl">
-                <li><NavLink to='/about'>About</NavLink></li>
-                <li><NavLink to='/music'>Music</NavLink></li>
-                <li><NavLink to='contact'>Contact</NavLink></li>
+                <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded'>About</NavLink></li>
+                <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded'>Music</NavLink></li>
+                <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded'>Contact</NavLink></li>
             </ul>
         </nav>
     )
