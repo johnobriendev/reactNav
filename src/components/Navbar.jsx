5c8427da55529d34a3df39f686a59ed908c8de5e
John O'Brien <johnobrien.dev@gmail.com>
2024-04-16 11:39:30 -0400

    change mobile nav to full screen, add close nav on link click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,16 @@ export default function Navbar(){
    const [menuOpen, setMenuOpen] = useState(false);
    
     return(
-        <nav className="flex flex-col items-start justify-between  bg-slate-900 text-white p-5 sticky top-0">
+        <nav className="flex flex-col items-start gap-5 md:flex-row md:justify-between md:items-center bg-slate-900 text-white p-5 sticky top-0">
             <Link to='/' className="text-3xl">Home</Link>
             
                 <div className="text-2xl md:hidden cursor-pointer absolute top-5 right-5" onClick={() => setMenuOpen(!menuOpen)}>
                     <RxHamburgerMenu />
                 </div>
-                <ul className={menuOpen ? "flex flex-col " : "hidden md:flex"}>
-                    <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded'>About</NavLink></li>
-                    <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded'>Music</NavLink></li>
-                    <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded'>Contact</NavLink></li>
+                <ul className={menuOpen ? "flex flex-col gap-5 w-screen h-screen " : "hidden md:flex"}>
+                    <li><NavLink to='/about' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(!menuOpen)}>About</NavLink></li>
+                    <li><NavLink to='/music' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(!menuOpen)}>Music</NavLink></li>
+                    <li><NavLink to='contact' className=' hover:bg-slate-700 p-2 rounded' onClick={() => setMenuOpen(!menuOpen)}>Contact</NavLink></li>
                 </ul>
            
             
