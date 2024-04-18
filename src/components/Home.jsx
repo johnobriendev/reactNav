0f7c255be63921e872759803d7232308ba3cf3d4
John O'Brien <johnobrien.dev@gmail.com>
2024-04-17 21:05:47 -0400

    add fake image and style to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,8 @@
 export default function Home(){
     return(
-        <div className="">Home</div>
+        <div className="w-full h-full flex flex-col gap-10 items-center py-10 px-2">
+            <h1 className="text-xl">This is the Homepage</h1>
+            <div className="h-[400px] min-w-[300px] max-w-[400px] bg-pink-300">This is a fake image</div>
+        </div>
     )
 }
