ed3fba31586f2e76ec74aa95a5984e863919b95c
John O'Brien <johnobrien.dev@gmail.com>
2024-04-18 22:03:55 -0400

    add contact form

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
-    <div className=''>
+    <div className='flex flex-col'>
       <Navbar />
       <Outlet />
       <Footer/>
