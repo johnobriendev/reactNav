e290b6b8282257961896372ff43ded44911dc2ae
John O'Brien <johnobrien.dev@gmail.com>
2024-04-18 22:17:00 -0400

    add video to music page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 export default function Footer() {
     return(
-        <div className="w-full h-[100px] bg-slate-800">This is the footer</div>
+        <div className="w-full h-[100px] bg-slate-800 bottom-0">This is the footer</div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ export default function Home(){
         <div className="w-full h-full flex flex-col gap-10 items-center py-10 px-2">
             <h1 className="text-xl">This is the Homepage</h1>
             <div className="h-[400px] min-w-[300px] max-w-[400px] bg-pink-300">This is a fake image</div>
+            <p className="p-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, nisi minus! Doloremque, ullam? Laboriosam, cum? Itaque praesentium eligendi optio laborum provident quasi est dolorem, a consectetur sed ratione unde perferendis similique magni impedit velit commodi aut architecto laudantium eos! Accusamus fugit tenetur sed iste est possimus facilis dolorum optio aspernatur.</p>
         </div>
     )
 }
