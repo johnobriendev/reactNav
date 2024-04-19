128df0ccf6858b961a6c20b6535670b27c2c637e
John O'Brien <johnobrien.dev@gmail.com>
2024-04-18 21:41:18 -0400

    create footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import { Outlet } from 'react-router-dom'
 
@@ -8,6 +9,7 @@ function App() {
     <div className=''>
       <Navbar />
       <Outlet />
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    return(
+        <div className="w-full h-[100px] bg-slate-800">This is the footer</div>
+    )
+}
