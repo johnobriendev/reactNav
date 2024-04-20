cb5b324fccb55708e680bab8913c176adad71fd0
John O'Brien <johnobrien.dev@gmail.com>
2024-04-19 23:08:29 -0400

    add hover effect to footer icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,9 @@ import { FaSpotify } from "react-icons/fa6";
 export default function Footer() {
     return(
         <div className=" flex items-center justify-center gap-20 w-full h-[100px] bg-slate-800 bottom-0 text-white text-3xl">
-            <FaInstagram />
-            <FaYoutube />
-            <FaSpotify />
+            <FaInstagram className="hover:scale-125 duration-150"/>
+            <FaYoutube className="hover:scale-125 duration-150" />
+            <FaSpotify className="hover:scale-125 duration-150" />
         </div>
     )
 }
