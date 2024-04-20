c0530d1da341ab80156fca326e39527cd4323975
John O'Brien <johnobrien.dev@gmail.com>
2024-04-19 23:03:09 -0400

    add icons to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
+import { FaInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+import { FaSpotify } from "react-icons/fa6";
+
+
+
+
 export default function Footer() {
     return(
-        <div className="w-full h-[100px] bg-slate-800 bottom-0">This is the footer</div>
+        <div className=" flex items-center justify-center gap-20 w-full h-[100px] bg-slate-800 bottom-0 text-white text-3xl">
+            <FaInstagram />
+            <FaYoutube />
+            <FaSpotify />
+        </div>
     )
 }
