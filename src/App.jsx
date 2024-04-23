f5ec54071ebcac4641c5816fa8477b4674053676
John O'Brien <johnobrien.dev@gmail.com>
2024-04-22 21:26:40 -0400

    remove example video from music page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
-    <div className='flex flex-col justify-between w-full h-auto md:h-screen'>
+    <div className='flex flex-col justify-between md:h-screen'>
       <Navbar />
       <Outlet />
       <Footer/>
--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -4,7 +4,7 @@ export default function Music(){
             <h2 className="text-2xl">Music</h2>
             <h3 className="text-xl">Discography</h3>
             <div className="flex my-10">
-                <iframe width="571" height="480" src="https://www.youtube.com/embed/k0Xc9i4v1ZE" title="#BVE #000 #BOLACA !" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                
             </div>
             
             
